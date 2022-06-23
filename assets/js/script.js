76ec52bf27a52d756ba2156ad25fdbc136e7461b
gabao55 <gabriel.s.rosin@gmail.com>
2022-06-22 22:07:28 -0300

    Implements send messages functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,6 @@ let user = {
     name: "PedroMóChavãoPataty"
 }
 const contactsTab = document.querySelector(".contacts");
-let messageBeingSent = {
-        from: name,
-        to: "Todos",
-        text: null,
-        type: null,
-    };
 const chat = document.querySelector(".chat");
 const participants = document.querySelector(".recipient");
 let messagesDisplayed = [];
@@ -20,19 +14,6 @@ function showContacts () {
 }
 
 function hideContacts () {
-    const recipientUl = contactsTab.querySelector(".recipient [name='checkmark-sharp']");
-    const recipient = recipientUl.parentNode.querySelector("p").innerText;
-    const isReservedUl = contactsTab.querySelector(".is-reserved [name='checkmark-sharp']");
-    const isReserved = isReservedUl.parentNode.querySelector("p").innerText;
-
-    messageBeingSent.recipient = recipient;
-
-    if (isReserved === "Público") {
-        messageBeingSent.isReserved = false;
-    } else {
-        messageBeingSent.isReserved = true;
-    }
-
     contactsTab.classList.add("display-none")
 }
 
@@ -53,7 +34,7 @@ function startApp () {
     // user.name = name;
     const promiseName = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", user);
     promiseName.then(displayApp);
-    promiseName.catch(nameFailed);
+    promiseName.catch(displayApp); // TODO: Trocar displayApp por nameFailed
 
 }
 
@@ -95,15 +76,38 @@ function getMessages (response) {
 function renderMessages (allMessages) {
     for (let i = 0 ; i < allMessages.length ; i ++) {
         messagesDisplayed.push(allMessages[i]);
-        const message = `
-            <li>
-            <p class="${allMessages[i].type}">
-                <span class="time">(${allMessages[i].time}) </span
-                ><span class="name">${allMessages[i].from} </span> ${allMessages[i].text}
-            </p>
-            </li>
-            <li>
-        `
+        let message;
+        if (allMessages[i].type === 'status') {
+            message = `
+                <li>
+                <p class="${allMessages[i].type}">
+                    <span class="time">(${allMessages[i].time}) </span
+                    ><span class="name">${allMessages[i].from}</span> ${allMessages[i].text}
+                </p>
+                </li>
+                <li>
+            `
+        } else if (allMessages[i].type === 'message') {
+            message = `
+                <li>
+                <p class="${allMessages[i].type}">
+                    <span class="time">(${allMessages[i].time}) </span
+                    ><span class="name">${allMessages[i].from}</span> para <span class="name">${allMessages[i].to} </span>${allMessages[i].text}
+                </p>
+                </li>
+                <li>
+            `
+        } else {
+            message = `
+                <li>
+                <p class="${allMessages[i].type}">
+                    <span class="time">(${allMessages[i].time}) </span
+                    ><span class="name">${allMessages[i].from}</span> reservadamente para <span class="name">${allMessages[i].to} </span>${allMessages[i].text}
+                </p>
+                </li>
+                <li>
+            `
+        }
         chat.innerHTML += message;
     }
 }
@@ -202,6 +206,49 @@ function ping() {
 
 function pingError (error) {
     alert("Conexão perdida, reinicie o chat.");
+}
+
+function sendMessage() {
+    const messageText = document.querySelector(".chat-bar input").value;
+    if (!messageText) {
+        return
+    }
+    const toUl = contactsTab.querySelector(".recipient [name='checkmark-sharp']");
+    const to = toUl.parentNode.querySelector("p").innerText;
+    const typeUl = contactsTab.querySelector(".is-reserved [name='checkmark-sharp']");
+    const type = typeUl.parentNode.querySelector("p").innerText;
+
+    let messageType;
+
+    if (type === "Público") {
+        messageType = "message";
+    } else {
+        messageType = "private_message";
+    }
+
+    const message = {
+        from: user.name,
+        to: to,
+        text: messageText,
+        type: messageType,
+    };
+
+    const promise = axios.post(
+        "https://mock-api.driven.com.br/api/v6/uol/messages",
+        message
+    )
+    promise.then(messageSentSuccessfully);
+    promise.catch(messageError);
+}
+
+function messageSentSuccessfully (response) {
+    console.log(response.status);
+    refreshMessages();
+}
+
+function messageError (error) {
+    console.log(error);
+    alert("Erro ao enviar mensagem, tente novamente");
 }
 
 startApp();
